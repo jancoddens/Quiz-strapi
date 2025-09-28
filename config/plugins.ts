07694be40a7c997ecb0598890af9dbd418e89763
Jan Coddens <jancoddens@MacBook-Pro-2.local>
2025-09-28 13:34:39 +0200

    Upgrade naar Strapi v5 + fixes

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,8 +1,21 @@
-// config/plugins.ts
-export default () => ({
-  rest: {
-    defaultLimit: 25,
-    maxLimit: 10000,   // ⬅️ VERHOGEN
-    withCount: true,
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp-auth.mailprotect.be"),
+        port: env.int("SMTP_PORT", 465),          // 587 = STARTTLS
+        secure: env.bool("SMTP_SECURE", true),   // true als je 465 gebruikt
+        auth: {
+          user: env("SMTP_USERNAME"),             // volledige mailbox, bv. noreply@jouwdomein.be
+          pass: env("SMTP_PASSWORD"),
+        },
+      },
+      settings: {
+        defaultFrom: env("EMAIL_FROM", "noreply@jouwdomein.be"),
+        defaultReplyTo: env("EMAIL_REPLY_TO", "support@jouwdomein.be"),
+      },
+    },
   },
+  rest: { defaultLimit: 25, maxLimit: 10000, withCount: true },
 });
