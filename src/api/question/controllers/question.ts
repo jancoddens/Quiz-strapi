340b04afc8cbf91f13cbd0ed70432e5908661943
Jan Coddens <jancoddens@MacBook-Pro-2.local>
2025-12-11 21:20:23 +0100

    Upgrade naar Strapi v5 + fixes

--- src/api/question/controllers/question.ts
+++ src/api/question/controllers/question.ts
@@ -1,33 +1,7 @@
-"use strict";
+/**
+ * question controller
+ */
 
-module.exports = {
-  async random(ctx) {
-    const table = "questions_question";
+import { factories } from '@strapi/strapi';
 
-    const min = await strapi.db.connection(table).min("id as min");
-    const max = await strapi.db.connection(table).max("id as max");
-
-    const minId = min[0].min;
-    const maxId = max[0].max;
-
-    if (!minId || !maxId) {
-      return ctx.send({ data: null });
-    }
-
-    const randomId =
-      Math.floor(Math.random() * (maxId - minId + 1)) + minId;
-
-    const row = await strapi.db.connection(table)
-      .select("slug")
-      .where("id", ">=", randomId)
-      .orderBy("id", "asc")
-      .limit(1);
-
-    const fallback = await strapi.db.connection(table)
-      .select("slug")
-      .orderBy("id", "asc")
-      .limit(1);
-
-    return ctx.send({ data: row[0] ?? fallback[0] });
-  },
-};
+export default factories.createCoreController('api::question.question');
